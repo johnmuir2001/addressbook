982f586838e05e442660e3a1319d24b56fb885d7
John Muir <johnmuir@Johns-MacBook-Pro.local>
2020-02-18 14:09:36 +0000

    working add contact and remove contact

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    list: [],
+    current: ""
+  };
+
+  handleEnter = event => {
+    console.log(event);
+    if (event.key === "Enter") {
+      this.submit();
+    }
+  };
+
+  handleInput = e => {
+    this.setState({ current: e.target.value });
+  };
+
+  submit = (e) => {
+    console.log("hi");
+    let storedNums = this.state.list;
+    if(this.state.current === ""){
+      return alert(`enter a value`)
+    }
+    storedNums.push(this.state.current);
+    this.setState({ list: storedNums, current: "" });
+  };
+
+  remove = (index) => {
+    let storedNums = this.state.list;
+    storedNums.splice(index, 1)
+    this.setState({list: storedNums})
+  }
+
+  render() {
+    return (
+      <div className="address">
+        <h1>Contacts</h1>
+        <div className="form">
+          <input
+            type="text"
+            onKeyPress={this.handleEnter}
+            onChange={this.handleInput}
+            value={this.state.current}
+            placeholder="e.g. 123-4567-8901"
+          ></input>
+          <button onClick={this.submit}>ADD</button>
+        </div>
+        <div className="contacts">
+          {this.state.list.map((number, index) => {
+            return <p key={index} onClick={() => this.remove(index)}>{number}</p>;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
